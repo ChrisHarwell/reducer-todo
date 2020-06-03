ccc01c3ff9ff183a8c15ed195956dd66144f7ad9
Chris <chrisharwell72@gmail.com>
2020-06-02 22:37:38 -0400

    All but the clear completed todos function is finished, need to add styling as well

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import React, { useState, useReducer } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
-const todoInitialState = {
-  item: "Learn about reducers",
-  completed: false,
-  id: uuidv4(),
-};
+const todoInitialState = [
+  {
+    item: "Learn about reducers",
+    completed: false,
+    id: uuidv4(),
+  },
+];
 const todoReducer = (state, action) => {
   switch (action.type) {
     case "TOGGLE_COMPLETED":
-      return {
-        ...state,
-        completed: !state.completed,
-      };
+      return state.map((item) =>
+        item.id === action.payload
+          ? { ...item, completed: !item.completed }
+          : item
+      );
+    // return {
+    //   ...state,
+    //   completed: !state.completed,
+    // };
     case "ADD_TODO":
-      return {
-        ...state,
-        item: action.payload,
-        completed: false,
-      };
+      return [...state, action.payload];
     default:
       return state;
   }
@@ -27,38 +30,49 @@ const todoReducer = (state, action) => {
 
 function App() {
   const [todoState, dispatch] = useReducer(todoReducer, todoInitialState);
-  const [newTodo, setNewTodo] = useState([""]);
+  const [newTodo, setNewTodo] = useState({
+    item: "",
+    completed: false,
+    id: uuidv4(),
+  });
 
   const handleChanges = (e) => {
-    setNewTodo(e.target.value);
+    setNewTodo({ ...newTodo, item: e.target.value });
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch({ type: "ADD_TODO", payload: newTodo });
-    // setNewTodo("");
+    setNewTodo({
+      item: "",
+      completed: false,
+      id: uuidv4(),
+    });
   };
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <input
           type="text"
-          name="newTodo"
-          // value={newTodo}
+          name="item"
+          value={newTodo.item}
           onChange={handleChanges}
         />
-        <button
-          onClick={() => {
-            dispatch({ type: "ADD_TODO", payload: newTodo });
-          }}
-        >
-          Update title
-        </button>
-        <input
-          type="radio"
-          onClick={() => {
-            dispatch({ type: "TOGGLE_COMPLETED" });
-          }}
-        ></input>
+        <button>Update title</button>
+        {todoState.map((item) => {
+          return (
+            <>
+              <input
+                type="checkbox"
+                checked={item.completed}
+                onClick={() => {
+                  dispatch({ type: "TOGGLE_COMPLETED", payload: item.id });
+                }}
+              />
+              <span>{item.item}</span>
+            </>
+          );
+        })}
+
         <pre>{JSON.stringify(todoState, null, 2)}</pre>
       </form>
     </div>
